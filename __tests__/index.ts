8c397518eec366df835c10b711efb1dfed0f1d42
Chris Cowan <agentme49@gmail.com>
2018-10-02 16:58:57 -0700

    +tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,1 +1,22 @@
-it('works', () => {});
+import depsCheck from '../src/function';
+
+import { resolve } from 'path';
+
+it('handles good case', () => {
+  depsCheck(resolve(__dirname, 'good'));
+});
+
+it('handles missing node_modules', () => {
+  expect(() => depsCheck(resolve(__dirname, 'noNodeModules'))).toThrow();
+});
+
+it('handles old node_modules', () => {
+  try {
+    depsCheck(resolve(__dirname, 'oldNodeModules'));
+    throw new Error('depsCheck did not throw');
+  } catch (e) {
+    expect(e.mismatch.length).toBe(7);
+    expect(e.inYarnLockOnly.length).toBe(7);
+    expect(e.inYarnIntegrityOnly.length).toBe(0);
+  }
+});
